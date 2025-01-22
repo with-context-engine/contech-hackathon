edbc23d0ce41de6533833513e42da182b1a04fc4
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-01-21 20:21:23 -0500

    adding a link

--- apps/web/src/components/shared/sidebar/_header/Heading.tsx
+++ apps/web/src/components/shared/sidebar/_header/Heading.tsx
@@ -5,15 +5,19 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@repo/ui/components/ui/sidebar';
+import Link from 'next/link';
 
 export function Heading() {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton size="lg">
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+          <Link
+            href="/"
+            className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
+          >
             <PaintbrushVertical className="size-4" />
-          </div>
+          </Link>
           <div className="grid flex-1 text-left text-sm leading-tight">
             <span className="truncate font-semibold">Beem</span>
             <span className="truncate text-xs">v1.0</span>
